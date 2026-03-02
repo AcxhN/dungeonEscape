561b6891761a9c2a280e9ece781ac689c3ed6723
Xavier <njsandilands@gmail.com>
2026-03-02 13:28:07 -0800

    added needed enums

---
+++ src/main/java/ca/sfu/cmpt276/team7/Endreason.java
@@ -0,0 +1,9 @@
+package main.java.ca.sfu.cmpt276.team7;
+
+public enum EndReason
+{
+    WIN,
+    LOSS_BY_TRAP,
+    LOSS_BY_OGRE,
+    LOSS_BY_GOBLIN
+}
---
+++ src/main/java/ca/sfu/cmpt276/team7/Popupreason.java
@@ -0,0 +1,7 @@
+package main.java.ca.sfu.cmpt276.team7;
+
+public enum Popupreason
+{
+    OGRE_HIT, 
+    BONUS_COLLECTED
+}
