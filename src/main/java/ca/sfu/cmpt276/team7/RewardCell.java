4ba328a060396b6c2abebfcf277e85dd4f4ba25a
Rambod <rpa89@sfu.ca>
2026-03-02 02:05:08 -0800

    Implement RewardCell

--- src/main/java/ca/sfu/cmpt276/team7/RewardCell.java
+++ src/main/java/ca/sfu/cmpt276/team7/RewardCell.java
@@ -1,5 +1,28 @@
 package ca.sfu.cmpt276.team7;
+/**
+ * A cell that contains a reward. When the player steps on this cell,
+ * the reward is collected and applied to the player's score.
+ */
+public class RewardCell extends Cell {
+   private Reward reward;
 
-public class RewardCell {
-
+ /**
+   * create a RewardCell at the given position with the specified reward.
+   * @param position the location of this cell on the board
+   * @param reward the reward contained in this cell
+   */
+   public RewardCell(Position position, Reward reward ){
+   super(position);
+   this.reward = reward;
+   }
+   public Reward getReward(){
+      return reward;
+   }
+   public boolean isWalkable(){
+      return true;
+   }
 }
+
+
+
+
