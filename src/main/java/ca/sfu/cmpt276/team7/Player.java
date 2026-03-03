fac5b5df89bcf879f453722995899eced17a9b52
Rambod <rpa89@sfu.ca>
2026-03-02 22:10:33 -0800

    add Player class not full

--- src/main/java/ca/sfu/cmpt276/team7/Player.java
+++ src/main/java/ca/sfu/cmpt276/team7/Player.java
@@ -1,5 +1,21 @@
 package ca.sfu.cmpt276.team7;
 
-public class Player {
+public class Player extends Character  {
+   private int totalScore;
+   private BonusReward activateBonus;
+
+   public Player(Board board, Position start){
+      super(board);
+      this.position = start;
+      this.totalScore = 0;
+      this.activateBonus = null;
+
+   }
+   public int getTotalScore(){
+      return totalScore;
+   }
+   
+
+
 
 }
