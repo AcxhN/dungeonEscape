68199e9ab9df4b9b12263142061e030e960b7468
Rambod <rpa89@sfu.ca>
2026-03-02 23:10:14 -0800

    Player class partial implementation

--- src/main/java/ca/sfu/cmpt276/team7/Player.java
+++ src/main/java/ca/sfu/cmpt276/team7/Player.java
@@ -1,5 +1,7 @@
 package ca.sfu.cmpt276.team7;
 
+import javax.swing.text.Position;
+
 public class Player extends Character  {
    private int totalScore;
    private BonusReward activateBonus;
@@ -16,7 +18,36 @@ public class Player extends Character  {
    }
    @override 
    public void move(Direction direction){
-      
+      Position newPos = null;
+      switch (direction){
+         case UP:
+            newPos = new Position(position.getX(), position.gettY() - 1);
+            break;
+         case Down:
+            newPos = new Position(position.getX(), position.getY() + 1);
+            break;
+         case LEFT:
+            newPos = new Position(position.getX() - 1, position.getY());
+            break;
+         case RIGHT:
+            newPos = new Position(position.getX() + 1, position.getY());
+            break; 
+
+      }
+      if (!board.isInside(newPos)) return;
+
+      Cell target = board.getCell(newPos);
+      if (!canMoveTo(target)) return;
+      this.position = newPos;
+
+      if (target instanceof RewardCell rewardcell){
+         collectReward(rewardCell.getReward());
+         rewardcell.clearReward();
+      }
+
+      if (target instanceof PunishmentCell punishmentCell){
+         applyPunishment(punishmentCell.getPunishment());
+      }
    }
    
 
