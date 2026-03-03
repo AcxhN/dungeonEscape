689a820b607d10c1dcb277c268c1d61dec8d9014
Rambod <rpa89@sfu.ca>
2026-03-02 23:36:40 -0800

    Player class implemented

--- src/main/java/ca/sfu/cmpt276/team7/Player.java
+++ src/main/java/ca/sfu/cmpt276/team7/Player.java
@@ -1,56 +1,83 @@
 package ca.sfu.cmpt276.team7;
 
-import javax.swing.text.Position;
+public class Player extends Character {
 
-public class Player extends Character  {
-   private int totalScore;
-   private BonusReward activateBonus;
+    private int totalScore;
+    private BonusReward activeBonus;
 
-   public Player(Board board, Position start){
-      super(board);
-      this.position = start;
-      this.totalScore = 0;
-      this.activateBonus = null;
+    public Player(Board board, Position start) {
+        super(board);
+        this.position = start;
+        this.totalScore = 0;
+        this.activeBonus = null;
+    }
 
-   }
-   public int getTotalScore(){
-      return totalScore;
-   }
-   @override 
-   public void move(Direction direction){
-      Position newPos = null;
-      switch (direction){
-         case UP:
-            newPos = new Position(position.getX(), position.gettY() - 1);
-            break;
-         case Down:
-            newPos = new Position(position.getX(), position.getY() + 1);
-            break;
-         case LEFT:
-            newPos = new Position(position.getX() - 1, position.getY());
-            break;
-         case RIGHT:
-            newPos = new Position(position.getX() + 1, position.getY());
-            break; 
+    public int getTotalScore() {
+        return totalScore;
+    }
 
-      }
-      if (!board.isInside(newPos)) return;
+    @Override
+    public void move(Direction direction) {
+        Position newPos = null;
 
-      Cell target = board.getCell(newPos);
-      if (!canMoveTo(target)) return;
-      this.position = newPos;
+        switch (direction) {
+            case UP:
+                newPos = new Position(position.getX(), position.getY() - 1);
+                break;
+            case DOWN:
+                newPos = new Position(position.getX(), position.getY() + 1);
+                break;
+            case LEFT:
+                newPos = new Position(position.getX() - 1, position.getY());
+                break;
+            case RIGHT:
+                newPos = new Position(position.getX() + 1, position.getY());
+                break;
+        }
 
-      if (target instanceof RewardCell rewardcell){
-         collectReward(rewardCell.getReward());
-         rewardcell.clearReward();
-      }
+        if (!board.isInside(newPos)) return;
 
-      if (target instanceof PunishmentCell punishmentCell){
-         applyPunishment(punishmentCell.getPunishment());
-      }
-   }
-   
+        Cell target = board.getCell(newPos);
+        if (!canMoveTo(target)) return;
 
+        this.position = newPos;
 
+        if (target instanceof RewardCell rewardCell) {
+            collectReward(rewardCell.getReward());
+            rewardCell.clearReward();
+        }
 
+        if (target instanceof PunishmentCell punishmentCell) {
+            applyPunishment(punishmentCell.getPunishment());
+        }
+    }
+
+    @Override
+    public boolean canMoveTo(Cell cell) {
+        return cell.isWalkable();
+    }
+
+    public void collectReward(Reward reward) {
+        if (reward instanceof RegularReward reg) {
+            totalScore += reg.getValue();
+        } else if (reward instanceof BonusReward bonus) {
+            this.activeBonus = bonus;
+        }
+    }
+
+    public void applyPunishment(Punishment punishment) {
+        totalScore -= punishment.getValue();
+        if (totalScore < 0) {
+            totalScore = 0;
+        }
+    }
+
+    public void tickBonus() {
+        if (activeBonus != null) {
+            activeBonus.tick();
+            if (activeBonus.getRemainingDuration() <= 0) {
+                activeBonus = null;
+            }
+        }
+    }
 }
