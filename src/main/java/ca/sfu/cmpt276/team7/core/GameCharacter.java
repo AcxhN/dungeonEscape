85bb9a93ddfe8ed9c8e65a559df51d6c251acfea
James <jam48@sfu.ca>
2026-03-05 13:08:58 -0800

    Added GameCharacter.java

---
+++ src/main/java/ca/sfu/cmpt276/team7/core/GameCharacter.java
@@ -0,0 +1,22 @@
+package ca.sfu.cmpt276.team7.core;
+
+import ca.sfu.cmpt276.team7.cells.Cell;
+import ca.sfu.cmpt276.team7.board.Board;
+import ca.sfu.cmpt276.team7.core.Postion;
+
+/**
+ * Base GameCharacter class
+ */
+
+abstract public class GameCharacter {
+    private Position position;
+    private Board board;
+
+    public GameCharacter(Board board){
+	this.board = board;
+    }
+    abstract public boolean canMoveto(Cell cell);
+    public Position getPosition() {
+	return position;
+    }
+}
