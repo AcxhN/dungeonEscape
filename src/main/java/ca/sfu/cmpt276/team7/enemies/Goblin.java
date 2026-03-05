18cba245fee04fa1a9e13f38e3f9fa68bdb231a7
James <jam48@sfu.ca>
2026-03-05 13:36:45 -0800

    Fixed import and public/private issues with enemies and gamecharacter

--- src/main/java/ca/sfu/cmpt276/team7/enemies/Goblin.java
+++ src/main/java/ca/sfu/cmpt276/team7/enemies/Goblin.java
@@ -1,5 +1,8 @@
-package main.java.ca.sfu.cmpt276.team7;
+package ca.sfu.cmpt276.team7.enemies;
 
+import ca.sfu.cmpt276.team7.core.Position; 
+import ca.sfu.cmpt276.team7.board.Board;
+import ca.sfu.cmpt276.team7.cells.*;
 /**
  * The goblin pathfinds towards the player
  * WIP, currently just a stub
@@ -7,12 +10,11 @@ package main.java.ca.sfu.cmpt276.team7;
 public class Goblin extends Enemy {
     
     public Goblin(Board board, Position pos) {
-	this.board = board;
-	this.position = pos;
+	super(board);
 	
     }
 
     public void updateMovement(Position player_position) {
-	
+
     }
 }
