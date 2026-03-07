2721e39e7c5cc1f040303772a353e48208834871
James <jam48@sfu.ca>
2026-03-07 15:57:16 -0800

    Get input listener working

--- src/main/java/ca/sfu/cmpt276/team7/ui/GameWindow.java
+++ src/main/java/ca/sfu/cmpt276/team7/ui/GameWindow.java
@@ -6,6 +6,8 @@ import ca.sfu.cmpt276.team7.Game;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * GameWindow opens the window, and starts the panel within it
@@ -21,6 +23,12 @@ public class GameWindow {
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		frame.addKeyListener(new KeyAdapter() {
+			public void  keyPressed(KeyEvent e) {
+			    game.handleInput(e.getKeyCode());
+			    panel.repaint();
+			}
+		    });
 	    });
     }
 }
