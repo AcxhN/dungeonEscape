5fcb391888e7da2a9b5be475a005f489084664ff
yma99 <yma99@sfu.ca>
2026-03-09 22:12:03 -0700

    Fix Javadoc for Punishment

--- src/main/java/ca/sfu/cmpt276/team7/reward/Punishment.java
+++ src/main/java/ca/sfu/cmpt276/team7/reward/Punishment.java
@@ -4,23 +4,31 @@ package ca.sfu.cmpt276.team7.reward;
  * Each punishment has a penalty value and defines behavior when triggered.
  */
 public abstract class Punishment{
+
+   /** Number of points removed when this punishment is applied. */
    protected int penaltyValue;
+
    /** 
     * Creates a punishment with the given penalty value. 
     * @param penaltyValue the number of points this punishment removes
    */
-
    public Punishment(int penaltyValue){
       this.penaltyValue = penaltyValue;
    }
+   
+   /**
+    * Returns the penalty value of this punishment.
+    *
+    * @return penalty value
+    */
    public int getPenaltyValue(){
       return penaltyValue;
    }
+   
    /**
      * Defines what happens when the player triggers this punishment.
      * Player.applyPunishment() handles score reduction; subclasses may add extra effects.
      */
-   
    public abstract void onTrigger();
 
 }
