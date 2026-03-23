0d1e951c1fe9a0e70b698f8e36435b956e11355c
acn8 <acn8@sfu.ca>
2026-03-23 14:50:46 -0700

    test(board): added setStartPosition out of bounds  case

--- src/test/java/ca/sfu/cmpt276/team7/unit/board/BoardTest.java
+++ src/test/java/ca/sfu/cmpt276/team7/unit/board/BoardTest.java
@@ -478,4 +478,21 @@ public class BoardTest {
             board.setStartPosition(null);
         });
     }
+
+    @Test
+    void setStartPosition_rejectsOutOfBounds() {
+        // Arrange
+        Cell[][] grid = new Cell[2][2];
+        grid[0][0] = new FloorCell(new Position(0, 0));
+        grid[0][1] = new FloorCell(new Position(1, 0));
+        grid[1][0] = new FloorCell(new Position(0, 1));
+        grid[1][1] = new FloorCell(new Position(1, 1));
+
+        Board board = new Board(grid);
+
+        // Act + Assert
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.setStartPosition(new Position(2, 0));
+        });
+    }
 }
