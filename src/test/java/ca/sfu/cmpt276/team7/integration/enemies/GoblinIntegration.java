f8a613254b68188d451f575b5c9ed9cdbe959bce
James <jam48@sfu.ca>
2026-03-24 15:52:07 -0700

    Fixed goblin integration test

--- src/test/java/ca/sfu/cmpt276/team7/integration/enemies/GoblinIntegration.java
+++ src/test/java/ca/sfu/cmpt276/team7/integration/enemies/GoblinIntegration.java
@@ -1,5 +1,6 @@
 package ca.sfu.cmpt276.team7.integration.enemies;
 
+import ca.sfu.cmpt276.team7.core.Direction;
 import ca.sfu.cmpt276.team7.core.Position;
 import ca.sfu.cmpt276.team7.board.Board;
 import ca.sfu.cmpt276.team7.cells.FloorCell;
@@ -8,6 +9,7 @@ import ca.sfu.cmpt276.team7.cells.Cell;
 import ca.sfu.cmpt276.team7.reward.PunishmentCell;
 import ca.sfu.cmpt276.team7.reward.TrapPunishment;
 import ca.sfu.cmpt276.team7.enemies.Goblin;
+import ca.sfu.cmpt276.team7.enemies.Ogre;
 import ca.sfu.cmpt276.team7.reward.Player;
 import ca.sfu.cmpt276.team7.enemies.Enemy;
 import ca.sfu.cmpt276.team7.Game;
@@ -23,9 +25,9 @@ class GoblinIntegrationTest{
     void TestGoblinPursuit() {
 	// Make board
 	Board board = new Board(new Cell[][] {
-		{ new FloorCell(new Position(0, 0)), new FloorCell(new Position(1, 0)) , new FloorCell(new Position(2, 0))}, 
-		{ new FloorCell(new Position(0, 1)), new WallCell(new Position(1, 1)) , new FloorCell(new Position(2, 1))},
-		{ new FloorCell(new Position(0, 2)), new FloorCell(new Position(1, 2)) , new FloorCell(new Position(2, 2))},
+		{ new FloorCell(new Position(0, 0)), new FloorCell(new Position(1, 0)) , new FloorCell(new Position(2, 0)), new FloorCell(new Position(3, 0))}, 
+		{ new FloorCell(new Position(0, 1)), new WallCell(new Position(1, 1)) , new PunishmentCell(new Position(2, 1), new TrapPunishment(1)), new FloorCell(new Position(3, 1))},
+		{ new FloorCell(new Position(0, 2)), new FloorCell(new Position(1, 2)) , new FloorCell(new Position(2, 2)), new FloorCell(new Position(3, 2))},
 
 	    });
 
@@ -38,6 +40,7 @@ class GoblinIntegrationTest{
 	enemies.add(goblin);
 
 	Game game = new Game(board, player, enemies, 0, 0, new ArrayList<>(), new ArrayList<>());
+	game.startGame();
 
 	// Test goblin against real player, using real game ticks
 	
@@ -59,13 +62,18 @@ class GoblinIntegrationTest{
 	assertTrue(goblin.getPosition().equals(new Position(0, 0)));
 
 	// Player moves, goblin chases
-	player.setPosition(new Position(2, 1));
+	player.setPosition(new Position(3, 0));
 	game.updateTick();
 	assertTrue(goblin.getPosition().equals(new Position(1, 0)));
 
-	// Player moves, goblin chases
-	player.setPosition(new Position(2, 2));
+	// an ogre spawns, blocking the path
+	Ogre ogre = new Ogre(board, new Position(2, 0), Direction.NORTH);
+	enemies.add(ogre);
+	
+	// Player moves, goblin should recognize the ogre blocking the path, and move backwards around
+	player.setPosition(new Position(3, 1));
 	game.updateTick();
-	assertTrue(goblin.getPosition().equals(new Position(2, 0)));
-    }
+	assertTrue(goblin.getPosition().equals(new Position(0, 0)));
+
+	}
 }
